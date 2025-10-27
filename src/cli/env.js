b1940ec7b5d061153d71192646f3d656554da6d6
pbhoper <155090999+pbhoper@users.noreply.github.com>
2025-10-27 03:21:14 +0300

    feat: file change

--- src/cli/env.js
+++ src/cli/env.js
@@ -1,10 +1,4 @@
 const parseEnv = () => {
-  const env = 
-        process.env;
-  const rss = 
-        Object.entries(env).filter(([key]) => key.startsWith("RSS_"));
-  const result =
-        rss.map(([key, value]) => `${key}=${value}`).join("; ");
-  console.log(result);
+  // Write your code here
 };
 parseEnv();
